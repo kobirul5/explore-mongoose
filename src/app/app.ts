ef6548d68cb794060a4d90328980a4ebe11b796f
Md. Kobirul Islam <kobirul05j@gmail.com>
2025-06-15 10:21:36 +0600

    find all note and find single not by id

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,18 +1,22 @@
 import express, { Application, Request, Response } from "express"
 import { model, Schema } from "mongoose"
 
-
-const app: Application  = express()
+const app: Application = express()
+app.use(express.json())
 
 const noteSchema = new Schema({
-  title: {type: String, required: true, trim:true},
-  content: {type: String, default: ""},
+  title: { type: String, required: true, trim: true },
+  content: { type: String, default: "" },
   category: {
     type: String,
     enum: ["personal", "work", "study", "other"],
     default: "personal"
   },
-  pinned: {type: Boolean, default: false}
+  pinned: { type: Boolean, default: false },
+  tags: {
+    label: { type: String, required: true },
+    color: { type: String, default: "gray" }
+  }
 
 
 })
@@ -20,22 +24,51 @@ const noteSchema = new Schema({
 const Note = model("Note", noteSchema);
 
 
-app.post('/create-note', async (req: Request, res: Response) => {
-  const myNote = new Note({
-    title: "Learning Mongoose",
-    
-  })
+app.post('/notes/create-note', async (req: Request, res: Response) => {
 
-  await myNote.save()
+  const body = req.body;
+
+  // Approach 1
+  // const myNote = new Note({
+  //   title: "Learning Mongoose",
+  // })
+  // await myNote.save()
+
+  // Approach 2
+
+  const note = await Note.create(body)
 
   res.status(201).json({
     success: true,
     message: "Note Created successfully",
-    note: myNote
+    note: note
   })
 
 
 
+})
+
+// All Nots fetch
+app.get('/notes/get-note', async (req: Request, res: Response) => {
+
+  const note = await Note.find()
+
+  res.status(201).json({
+    success: true,
+    message: "Note Created successfully",
+    note: note
+  })
+})
+// find  Note by id fetch
+app.get('/note/get-note/:noteId', async (req: Request, res: Response) => {
+  const id = req.params.noteId
+  const note = await Note.findById(id)
+
+  res.status(201).json({
+    success: true,
+    message: "Note Created successfully",
+    note: note
+  })
 })
 
 app.get('/', (req: Request, res: Response) => {
